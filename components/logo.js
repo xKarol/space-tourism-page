1cb75a388abf9e7814e578dcb29e1303989e9a52
xKarol <karolpiskorz8@gmail.com>
2022-02-06 12:30:54 +0100

    Fixed logo console error

--- components/logo.js
+++ components/logo.js
@@ -3,13 +3,15 @@ import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Link href="/" passHref>
-      <Image
-        src={"/assets/shared/logo.svg"}
-        alt="space tourism logo"
-        width={48}
-        height={48}
-      />
+    <Link href="/">
+      <a className="logo" aria-label="space tourism home">
+        <Image
+          src={"/assets/shared/logo.svg"}
+          alt="space tourism logo"
+          width={48}
+          height={48}
+        />
+      </a>
     </Link>
   );
 }

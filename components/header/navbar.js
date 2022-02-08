1804d013d7fec045ad6ca370d66a920f1a7fbcf5
xKarol <karolpiskorz8@gmail.com>
2022-02-08 14:08:36 +0100

    Add hamburger menu slide animation, hide scroll in active hamburger

--- components/header/navbar.js
+++ components/header/navbar.js
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavbarContext from "../../context/NavbarContext";
 import Hamburger from "./hamburger";
 import NavbarLink from "./navbar-link";
 
 export default function Navbar() {
   const [hamburger, setHamburger] = useState(false);
+
+  useEffect(() => {
+    if (hamburger) {
+      window.scrollTo(0, 0);
+      document.body.classList.add("no-scroll");
+    }
+    return () => document.body.classList.remove("no-scroll");
+  }, [hamburger]);
+  
   return (
     <NavbarContext.Provider value={{ hamburger, setHamburger }}>
       <nav className={`navbar ${hamburger ? "hamburger-menu" : ""}`}>
